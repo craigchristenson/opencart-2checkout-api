4974073407e78afb0ffa29addd5297bbd61512c5
xxvirusxx <condor20_05@yahoo.it>
2019-11-12 17:41:43 +0200

    Update twocheckout_api.php

--- catalog/model/extension/payment/twocheckout_api.php
+++ catalog/model/extension/payment/twocheckout_api.php
@@ -5,9 +5,9 @@ class ModelExtensionPaymentTwoCheckoutApi extends Model {
 		
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('twocheckout_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 		
-		if ($this->config->get('twocheckout_api_total') > 0 && $this->config->get('twocheckout_api_total') > $total) {
+		if ($this->config->get('payment_twocheckout_api_total') > 0 && $this->config->get('payment_twocheckout_api_total') > $total) {
 			$status = false;
-		} elseif (!$this->config->get('twocheckout_api_geo_zone_id')) {
+		} elseif (!$this->config->get('payment_twocheckout_api_geo_zone_id')) {
 			$status = true;
 		} elseif ($query->num_rows) {
 			$status = true;
@@ -19,14 +19,13 @@ class ModelExtensionPaymentTwoCheckoutApi extends Model {
 	
 		if ($status) {  
       		$method_data = array( 
-        		'code'       => 'twocheckout_api',
+        		'code'       => 'payment_twocheckout_api',
         		'title'      => $this->language->get('text_title'),
         		'terms'      => '',
-				'sort_order' => $this->config->get('twocheckout_api_sort_order')
+			'sort_order' => $this->config->get('payment_twocheckout_api_sort_order')
       		);
     	}
    
     	return $method_data;
   	}
 }
-?>
