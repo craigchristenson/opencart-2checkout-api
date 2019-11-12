9800906053143f681b066462a12625ad4513c9ce
xxvirusxx <condor20_05@yahoo.it>
2019-11-12 17:21:55 +0200

    Update twocheckout_pp.php

--- admin/controller/extension/payment/twocheckout_pp.php
+++ admin/controller/extension/payment/twocheckout_pp.php
@@ -10,31 +10,12 @@ class ControllerExtensionPaymentTwoCheckoutPP extends Controller {
 		$this->load->model('setting/setting');
 			
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			$this->model_setting_setting->editSetting('twocheckout_pp', $this->request->post);				
+			$this->model_setting_setting->editSetting('payment_twocheckout_pp', $this->request->post);				
 			
 			$this->session->data['success'] = $this->language->get('text_success');
 
-			$this->response->redirect($this->url->link('extension/extension', 'token=' . $this->session->data['token'], true));
+			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'], true));
 		}
-
-		$data['heading_title'] = $this->language->get('heading_title');
-
-		$data['text_enabled'] = $this->language->get('text_enabled');
-		$data['text_disabled'] = $this->language->get('text_disabled');
-		$data['text_all_zones'] = $this->language->get('text_all_zones');
-		$data['text_yes'] = $this->language->get('text_yes');
-		$data['text_no'] = $this->language->get('text_no');
-		
-		$data['entry_account'] = $this->language->get('entry_account');
-		$data['entry_secret'] = $this->language->get('entry_secret');
-		$data['entry_total'] = $this->language->get('entry_total');	
-		$data['entry_order_status'] = $this->language->get('entry_order_status');		
-		$data['entry_geo_zone'] = $this->language->get('entry_geo_zone');
-		$data['entry_status'] = $this->language->get('entry_status');
-		$data['entry_sort_order'] = $this->language->get('entry_sort_order');
-		
-		$data['button_save'] = $this->language->get('button_save');
-		$data['button_cancel'] = $this->language->get('button_cancel');
 		 
 		if (isset($this->error['warning'])) {
 			$data['error_warning'] = $this->error['warning'];
@@ -57,69 +38,66 @@ class ControllerExtensionPaymentTwoCheckoutPP extends Controller {
   		$data['breadcrumbs'] = array();
 
    		$data['breadcrumbs'][] = array(
-       		'text'      => $this->language->get('text_home'),
-			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),       		
-      		'separator' => false
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true),       		
    		);
 
    		$data['breadcrumbs'][] = array(
-       		'text'      => $this->language->get('text_payment'),
-			'href'      => $this->url->link('extension/extension', 'token=' . $this->session->data['token'], 'SSL'),
-      		'separator' => ' :: '
+			'text' => $this->language->get('text_extension'),
+			'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true),
    		);
 
    		$data['breadcrumbs'][] = array(
-       		'text'      => $this->language->get('heading_title'),
-			'href'      => $this->url->link('extension/payment/twocheckout_pp', 'token=' . $this->session->data['token'], 'SSL'),
-      		'separator' => ' :: '
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('extension/payment/twocheckout_pp', 'user_token=' . $this->session->data['user_token'], true),
    		);
 				
-		$data['action'] = $this->url->link('extension/payment/twocheckout_pp', 'token=' . $this->session->data['token'], 'SSL');
+		$data['action'] = $this->url->link('extension/payment/twocheckout_pp', 'user_token=' . $this->session->data['user_token'], true);
 		
-		$data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'], 'SSL');
+		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=payment', true);
 		
-		if (isset($this->request->post['twocheckout_pp_account'])) {
-			$data['twocheckout_pp_account'] = $this->request->post['twocheckout_pp_account'];
+		if (isset($this->request->post['payment_twocheckout_pp_account'])) {
+			$data['payment_twocheckout_pp_account'] = $this->request->post['payment_twocheckout_pp_account'];
 		} else {
-			$data['twocheckout_pp_account'] = $this->config->get('twocheckout_pp_account');
+			$data['payment_twocheckout_pp_account'] = $this->config->get('payment_twocheckout_pp_account');
 		}
 
-		if (isset($this->request->post['twocheckout_pp_secret'])) {
-			$data['twocheckout_pp_secret'] = $this->request->post['twocheckout_pp_secret'];
+		if (isset($this->request->post['payment_twocheckout_pp_secret'])) {
+			$data['payment_twocheckout_pp_secret'] = $this->request->post['payment_twocheckout_pp_secret'];
 		} else {
-			$data['twocheckout_pp_secret'] = $this->config->get('twocheckout_pp_secret');
+			$data['payment_twocheckout_pp_secret'] = $this->config->get('payment_twocheckout_pp_secret');
 		}
 		
-		if (isset($this->request->post['twocheckout_pp_total'])) {
-			$data['twocheckout_pp_total'] = $this->request->post['twocheckout_pp_total'];
+		if (isset($this->request->post['payment_twocheckout_pp_total'])) {
+			$data['payment_twocheckout_pp_total'] = $this->request->post['payment_twocheckout_pp_total'];
 		} else {
-			$data['twocheckout_pp_total'] = $this->config->get('twocheckout_pp_total'); 
+			$data['payment_twocheckout_pp_total'] = $this->config->get('payment_twocheckout_pp_total'); 
 		} 
 				
-		if (isset($this->request->post['twocheckout_pp_order_status_id'])) {
-			$data['twocheckout_pp_order_status_id'] = $this->request->post['twocheckout_pp_order_status_id'];
+		if (isset($this->request->post['payment_twocheckout_pp_order_status_id'])) {
+			$data['payment_twocheckout_pp_order_status_id'] = $this->request->post['payment_twocheckout_pp_order_status_id'];
 		} else {
-			$data['twocheckout_pp_order_status_id'] = $this->config->get('twocheckout_pp_order_status_id'); 
+			$data['payment_twocheckout_pp_order_status_id'] = $this->config->get('payment_twocheckout_pp_order_status_id'); 
 		}
 		
 		$this->load->model('localisation/order_status');
 		
 		$data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
 		
-		if (isset($this->request->post['twocheckout_pp_geo_zone_id'])) {
-			$data['twocheckout_pp_geo_zone_id'] = $this->request->post['twocheckout_pp_geo_zone_id'];
+		if (isset($this->request->post['payment_twocheckout_pp_geo_zone_id'])) {
+			$data['payment_twocheckout_pp_geo_zone_id'] = $this->request->post['payment_twocheckout_pp_geo_zone_id'];
 		} else {
-			$data['twocheckout_pp_geo_zone_id'] = $this->config->get('twocheckout_pp_geo_zone_id'); 
+			$data['payment_twocheckout_pp_geo_zone_id'] = $this->config->get('payment_twocheckout_pp_geo_zone_id'); 
 		}
 		
 		$this->load->model('localisation/geo_zone');
 										
 		$data['geo_zones'] = $this->model_localisation_geo_zone->getGeoZones();
 		
-		if (isset($this->request->post['twocheckout_pp_status'])) {
-			$data['twocheckout_pp_status'] = $this->request->post['twocheckout_pp_status'];
+		if (isset($this->request->post['payment_twocheckout_pp_status'])) {
+			$data['payment_twocheckout_pp_status'] = $this->request->post['payment_twocheckout_pp_status'];
 		} else {
-			$data['twocheckout_pp_status'] = $this->config->get('twocheckout_pp_status');
+			$data['payment_twocheckout_pp_status'] = $this->config->get('payment_twocheckout_pp_status');
 		}
 		
 		if (isset($this->request->post['twocheckout_pp_sort_order'])) {
@@ -140,19 +118,14 @@ class ControllerExtensionPaymentTwoCheckoutPP extends Controller {
 			$this->error['warning'] = $this->language->get('error_permission');
 		}
 		
-		if (!$this->request->post['twocheckout_pp_account']) {
+		if (!$this->request->post['payment_twocheckout_pp_account']) {
 			$this->error['account'] = $this->language->get('error_account');
 		}
 
-		if (!$this->request->post['twocheckout_pp_secret']) {
+		if (!$this->request->post['payment_twocheckout_pp_secret']) {
 			$this->error['secret'] = $this->language->get('error_secret');
 		}
 		
-		if (!$this->error) {
-			return true;
-		} else {
-			return false;
-		}	
+		return !$this->error;
 	}
 }
-?>
